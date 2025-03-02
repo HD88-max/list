3069e6d2610d706973a80c2815a0979938106bca
HD88-max <hardikdhallam@gmail.com>
2025-03-02 16:14:03 +0000

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+#Write a program to perform the following operations on a List: 1. Create an empty list 2. A list with elements 3. Use * operator 4. Reverse a list
+
+emptylist = []
+print(emptylist)
+
+elementlist = [100,2,"Hello","bye",True,False,3.22]
+print(elementlist)
+
+operatorlist = [23,13]*3
+print(operatorlist)
+
+reverselist = [1,2,3,"hello",True]
+reverselist.reverse()
+print(reverselist)
